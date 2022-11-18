f682388fedd441899b52571582b00d94528707d0
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2022-11-18 16:20:46 +0200

    strip-updates fix: input + button + checkbox work appropriately with email validation

--- scripts/index.js
+++ scripts/index.js
@@ -2,12 +2,30 @@ const form = document.getElementById("form")
 const agree = document.querySelector("#agree")
 const stripUpdatesBtn = document.getElementById("submit")
 const stripUpdatesInput = document.getElementById("strip-updates__text_input")
+const reg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
-agree.oninput = function () {
+
+agree.oninput = function (event) {
     if (agree.checked) {
         stripUpdatesBtn.removeAttribute('disabled')
 
     } else {
         stripUpdatesBtn.setAttribute('disabled', false)
     }
+}
+
+
+stripUpdatesBtn.onclick = function(event){
+    event.preventDefault();
+
+    if(!validate(reg,stripUpdatesInput.value)){
+        return;
+    }
+    else {
+      alert("work")
+    }
+}
+
+function validate(regex,inp){
+    return regex.test(inp)
 }
