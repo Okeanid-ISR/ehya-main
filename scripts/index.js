84fbe55c4c4bc40dffc871a0b8b4b558f46c40e9
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2022-11-25 00:04:38 +0200

    strip-updated feat: styles added

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,39 @@ const form = document.getElementById("form")
 const agree = document.getElementById("agree")
 const submit = document.getElementById("submit")
 const stripUpdatesInput = document.getElementById("str-form__input")
+const pic = document.getElementById("pic")
 const reg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 
 agree.oninput = function (event) {
+
     if (agree.checked) {
+        submit.style.opacity = "100%"
         submit.removeAttribute('disabled')
+        submit.style.cursor = 'pointer'
+        pic.style.fill = 'blue'
+
 
     } else {
         submit.setAttribute('disabled', false)
+        submit.style.cursor = 'not-allowed'
+        submit.style.opacity = '70%'
+        pic.style.fill = ''
     }
 }
 
 
-submit.onclick = function(event){
+submit.onclick = function (event) {
     event.preventDefault();
 
-    if(!validate(reg,stripUpdatesInput.value)){
+    if (!validate(reg, stripUpdatesInput.value)) {
+
         return;
-    }
-    else {
+    } else {
       alert("work")
     }
 }
 
-function validate(regex,inp){
+function validate(regex, inp) {
     return regex.test(inp)
 }
