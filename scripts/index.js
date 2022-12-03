358092d17ea96d0aed9d70365ff32d6966557d6a
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2022-12-03 03:22:08 +0200

    adaptive for header

--- scripts/index.js
+++ scripts/index.js
@@ -3,33 +3,45 @@ const agree = document.getElementById("agree")
 const submit = document.getElementById("submit")
 const stripUpdatesInput = document.getElementById("str-form-input")
 const reg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const menu = document.getElementById("header-button")
+const headerMenu = document.getElementById("header-menu")
+const headerMenuActive = 'header-menu_opened'
+
+if (form) {
+    agree.oninput = function (event) {
+
+        if (agree.checked) {
+            submit.style.opacity = "100%"
+            submit.removeAttribute('disabled')
+            submit.style.cursor = 'pointer'
+        } else {
+            submit.setAttribute('disabled', false)
+            submit.style.cursor = 'not-allowed'
+            submit.style.opacity = '70%'
+        }
+    }
 
 
-agree.oninput = function (event) {
+    submit.onclick = function (event) {
+        event.preventDefault();
 
-    if (agree.checked) {
-        submit.style.opacity = "100%"
-        submit.removeAttribute('disabled')
-        submit.style.cursor = 'pointer'
-    } else {
-        submit.setAttribute('disabled', false)
-        submit.style.cursor = 'not-allowed'
-        submit.style.opacity = '70%'
+        if (!validate(reg, stripUpdatesInput.value)) {
+
+            return;
+        } else {
+            alert("work")
+        }
+    }
+
+    function validate(regex, inp) {
+        return regex.test(inp)
     }
 }
 
-
-submit.onclick = function (event) {
-    event.preventDefault();
-
-    if (!validate(reg, stripUpdatesInput.value)) {
-
-        return;
-    } else {
-      alert("work")
+menu.addEventListener('click', function (event) {
+    if(!headerMenu.classList.contains(headerMenuActive)){
+        headerMenu.classList.add("header-menu_opened")
+    }else{
+        headerMenu.classList.remove("header-menu_opened")
     }
-}
-
-function validate(regex, inp) {
-    return regex.test(inp)
-}
+        })
